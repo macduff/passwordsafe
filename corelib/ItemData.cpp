f85bf2ac891ef4bb2d3d78552a8665321bdcbbdc
ronys <ronys@6718adc6-b60d-0410-b017-daa5ac3f2f0f>
2007-08-22 15:24:47 +0000

    Finished refactoring started in last commit - ready for merging back to trunk.

--- corelib/ItemData.cpp
+++ corelib/ItemData.cpp
@@ -1221,41 +1221,34 @@ CItemData::Matches(const CString &subgroup_name, int iObject,
 }
 
 static bool
-pull_string(CMyString &str, vector<char>::const_iterator &iter,
-            size_t len)
+pull_string(CMyString &str, unsigned char *data, size_t len)
 {
   CUTF8Conv utf8conv;
-  vector<char> v(iter, (iter + len));
+  vector<unsigned char> v(data, (data + len));
   v.push_back(0); // null terminate for FromUTF8.
   bool utf8status = utf8conv.FromUTF8((unsigned char *)&v[0],
                                       len, str);
   if (!utf8status) {
     TRACE(_T("CItemData::DeserializePlainText(): FromUTF8 failed!\n"));
   }
-  iter += len;
   trashMemory(&v[0], len);
   return utf8status;
 }
 
 static bool
-pull_time(time_t &t, vector<char>::const_iterator &iter,
-          size_t len)
+pull_time(time_t &t, unsigned char *data, size_t len)
 {
   if (len != sizeof(t)) {
     ASSERT(0);
     return false;
   }
-  t = *((time_t *)&(*iter));
-  iter += len;
+  t = *((time_t *)data);
   return true;
 }
 
 bool CItemData::DeserializePlainText(const std::vector<char> &v)
 {
   vector<char>::const_iterator iter = v.begin();
-  bool retval = false; // true when title set
-  CMyString str;
-  time_t t;
   int emergencyExit = 255;
 
   while (iter != v.end()) {
@@ -1264,6 +1257,10 @@ bool CItemData::DeserializePlainText(const std::vector<char> &v)
       ASSERT(0); // type must ALWAYS be followed by length
       return false;
     }
+
+    if (type == END)
+      return true; // happy end
+
     size_t len = *((size_t *)&(*iter));
     ASSERT(len < v.size()); // sanity check
     iter += sizeof(size_t);
@@ -1272,88 +1269,95 @@ bool CItemData::DeserializePlainText(const std::vector<char> &v)
       ASSERT(0);
       return false;
     }
-
-    switch (type) {
-    case NAME:
-      ASSERT(0); // not serialized, so can't get here
+    if (!SetField(type, (unsigned char *)&(*iter), len))
       return false;
-    case UUID:
-      {
-        uuid_array_t uuid_array;
-        ASSERT(len == sizeof(uuid_array));
-        for (unsigned i = 0; i < sizeof(uuid_array); i++)
-          uuid_array[i] = *iter++;
-        SetUUID(uuid_array);
-      }
-      break;
-    case GROUP:
-      if (!pull_string(str, iter, len)) return false;
-      SetGroup(str);
-      break;
-    case TITLE:
-      if (!pull_string(str, iter, len)) return false;
-      retval = true;
-      SetTitle(str);
-      break;
-    case USER:
-      if (!pull_string(str, iter, len)) return false;
-      SetUser(str);
-      break;
-    case NOTES:
-      if (!pull_string(str, iter, len)) return false;
-      SetNotes(str);
-      break;
-    case PASSWORD:
-      if (!pull_string(str, iter, len)) return false;
-      SetPassword(str);
-      break;
-    case CTIME:
-      if (!pull_time(t, iter, len)) return false;
-      SetCTime(t);
-      break;
-    case  PMTIME:
-      if (!pull_time(t, iter, len)) return false;
-      SetPMTime(t);
-      break;
-    case ATIME:
-      if (!pull_time(t, iter, len)) return false;
-      SetATime(t);
-      break;
-    case LTIME:
-      if (!pull_time(t, iter, len)) return false;
-      SetLTime(t);
-      break;
-    case POLICY:
-      ASSERT(0); // not serialized, so can't get here
-      return false;
-    case RMTIME:
-      if (!pull_time(t, iter, len)) return false;
-      SetRMTime(t);
-      break;
-    case URL:
-      if (!pull_string(str, iter, len)) return false;
-      SetURL(str);
-      break;
-    case AUTOTYPE:
-      if (!pull_string(str, iter, len)) return false;
-      SetAutoType(str);
-      break;
-    case PWHIST:
-      if (!pull_string(str, iter, len)) return false;
-      SetPWHistory(str);
-      break;
-    case END:
-      goto done;
-    default:
-      // unknowns!
-      SetUnknownField(char(type), len, (unsigned char *)&(*iter));
-      iter += len;
-      break;
-    }
+    iter += len;
   }
- done:
-  return retval;
+  return false; // END tag not found!
 }
+
+bool CItemData::SetField(int type, unsigned char *data, size_t len)
+{
+  CMyString str;
+  time_t t;
+  switch (type) {
+  case NAME:
+    ASSERT(0); // not serialized, or in v3 format
+    return false;
+  case UUID:
+    {
+      uuid_array_t uuid_array;
+      ASSERT(len == sizeof(uuid_array));
+      for (unsigned i = 0; i < sizeof(uuid_array); i++)
+        uuid_array[i] = data[i];
+      SetUUID(uuid_array);
+    }
+    break;
+  case GROUP:
+    if (!pull_string(str, data, len)) return false;
+    SetGroup(str);
+    break;
+  case TITLE:
+    if (!pull_string(str, data, len)) return false;
+    SetTitle(str);
+    break;
+  case USER:
+    if (!pull_string(str, data, len)) return false;
+    SetUser(str);
+    break;
+  case NOTES:
+    if (!pull_string(str, data, len)) return false;
+    SetNotes(str);
+    break;
+  case PASSWORD:
+    if (!pull_string(str, data, len)) return false;
+    SetPassword(str);
+    break;
+  case CTIME:
+    if (!pull_time(t, data, len)) return false;
+    SetCTime(t);
+    break;
+  case  PMTIME:
+    if (!pull_time(t, data, len)) return false;
+    SetPMTime(t);
+    break;
+  case ATIME:
+    if (!pull_time(t, data, len)) return false;
+    SetATime(t);
+    break;
+  case LTIME:
+    if (!pull_time(t, data, len)) return false;
+    SetLTime(t);
+    break;
+  case POLICY:
+    ASSERT(0); // not used, so can't get here
+    return false;
+  case RMTIME:
+    if (!pull_time(t, data, len)) return false;
+    SetRMTime(t);
+    break;
+  case URL:
+    if (!pull_string(str, data, len)) return false;
+    SetURL(str);
+    break;
+  case AUTOTYPE:
+    if (!pull_string(str, data, len)) return false;
+    SetAutoType(str);
+    break;
+  case PWHIST:
+    if (!pull_string(str, data, len)) return false;
+    SetPWHistory(str);
+    break;
+  case END:
+    break;
+  default:
+    // unknowns!
+    SetUnknownField(char(type), len, data);
+    break;
+  }
+  return true;
+}
+
 
 static void
 push_length(vector<char> &v, size_t s)
